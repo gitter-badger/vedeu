e3d4fd428e97afeae707b008e2bf3d9217f6dc06
Gavin Laking <gavinlaking@gmail.com>
2014-11-08 23:24:48 +0000

    Start refactoring colour aspect of API/DSL.

--- lib/vedeu/api/helpers.rb
+++ lib/vedeu/api/helpers.rb
@@ -10,119 +10,66 @@ module Vedeu
 
       include Common
 
-      # Define the background colour for an interface, line, or a stream. When
-      # called with a block, will create a new stream with the background colour
-      # specified; see {Vedeu::API::Stream} for directives which are valid
-      # within the block. When the block terminates, the background will return
-      # to that of the parent.
+      # Define either or both foreground and background colours for an
+      # interface, line, or a stream. When called with a block, will create a
+      # new stream with the background/foreground colour specified; see
+      # {Vedeu::API::Stream} for directives which are valid within the block.
+      # When the block terminates, the background/foreground will return to that
+      # of the parent.
       #
-      # @param value [String]
+      # @param value [Hash|String]
       # @param block [Proc]
       #
       # @example
       #   interface 'my_interface' do
-      #     background '#0022ff'
-      #     ...
-      #
-      #   line do
-      #     background '#0022ff'
-      #     ...
-      #
-      #   stream do
-      #     background '#0022ff'
-      #     ...
-      #
-      #   foreground('#0022ff') { 'This will be blue.' }
-      #
-      # @raise [InvalidSyntax] When the value is not defined.
-      # @return [Hash]
-      def background(value = '', &block)
-        unless defined_value?(value)
-          fail InvalidSyntax, '`background` requires a value.'
-        end
-
-        if block_given?
-          attributes[:streams] << API::Stream
-            .build({ colour: { background: value },
-                     parent: self.view_attributes }, &block)
-
-        else
-          attributes[:colour].merge!({ background: value })
-
-        end
-      end
-
-      # Define either or both foreground and background colours for an
-      # interface, line or a stream.
-      #
-      # @param values [Hash]
-      #
-      # @example
-      #   interface 'my_interface' do
       #     colour background: '#000000', foreground: '#ffffff'
+      #     background '#0022ff'
+      #     foreground '#ffffff'
       #     ...
       #
       #   line do
       #     colour background: '#000000', foreground: '#ffffff'
+      #     background '#0022ff'
+      #     foreground '#ffffff'
       #     ...
       #
       #   stream do
       #     colour background: '#000000', foreground: '#ffffff'
+      #     background '#0022ff'
+      #     foreground '#ffffff'
       #     ...
+      #
+      #   background('#0022ff') { 'This will be on a blue background.' }
+      #   foreground('#ffffff') { 'This will be white text.' }
       #
       # @raise [InvalidSyntax] When the values parameter does not contain
-      #   required keys.
+      #   required keys, or a non-empty String value.
       # @return [Hash]
-      def colour(values)
-        unless values.key?(:foreground) || values.key?(:background)
-          fail InvalidSyntax, '#colour expects a Hash containing ' \
-            ':foreground or :background or both.'
-        end
+      def colour(value, &block)
+        fail InvalidSyntax, "##{__callee__} requires a value." unless defined_value?(value)
 
-        attributes[:colour] = values
-      end
+        if value.is_a?(Hash)
+          unless value.key?(:foreground) || value.key?(:background)
+            fail InvalidSyntax, "##{__callee__} expects a Hash containing " \
+              ':foreground or :background or both.'
+          end
 
-      # Define the foreground colour for an interface, line, or a stream. When
-      # called with a block, will create a new stream with the foreground colour
-      # specified; see {Vedeu::API::Stream} for directives which are valid
-      # within the block. When the block terminates, the foreground will return
-      # to that of the parent.
-      #
-      # @param value [String]
-      # @param block [Proc]
-      #
-      # @example
-      #   interface 'my_interface' do
-      #     foreground '#0022ff'
-      #     ...
-      #
-      #   line do
-      #     foreground '#0022ff'
-      #     ...
-      #
-      #   stream do
-      #     foreground '#0022ff'
-      #     ...
-      #
-      #   foreground('#0022ff') { 'This will be blue.' }
-      #
-      # @raise [InvalidSyntax] When the value is not defined.
-      # @return [Hash]
-      def foreground(value = '', &block)
-        unless defined_value?(value)
-          fail InvalidSyntax, '`foreground` requires a value.'
-        end
-
-        if block_given?
-          attributes[:streams] << API::Stream
-            .build({ colour: { foreground: value },
-                     parent: self.view_attributes }, &block)
+          attributes[:colour] = value
 
         else
-          attributes[:colour].merge!({ foreground: value })
+          if block_given?
+            attributes[:streams] << API::Stream
+              .build({ colour: { __callee__ => value },
+                       parent: self.view_attributes }, &block)
 
+          else
+            attributes[:colour].merge!({ __callee__ => value })
+
+          end
         end
       end
+      alias_method :background, :colour
+      alias_method :foreground, :colour
 
       # Define a style or styles for an interface, line or a stream.
       #
